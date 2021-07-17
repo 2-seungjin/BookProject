640fe7eaa161a2940016b6c13f37b61984fda53e
leeseungjin <dltmdwls154@gmail.com>
2021-07-17 17:27:54 +0900

    Todo선능 분석을 위해 많은 TodoList 생성 함수  작성

--- src/App.js
+++ src/App.js
@@ -3,26 +3,21 @@ import TodoTemplate from './components/TodoTemplate'
 import TodoInsert from './components/TodoInsert'
 import TodoList from './components/TodoList'
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id:1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id:2,
-      text: '컴포넌트 스타일링해 보기',
-      checked: true,
-    },
-    {
-      id:3,
-      text: '일정 관리 앱 만들어 보기',
-      checked: false,
-    },
-  ]);
+  const createBulkTodos = () => {
+    const array = [];
+    for(let a = 1; a <= 2500; a++){
+      array.push({
+        id:a,
+        text:`할일 ${a}`,
+        checked: false,
+      });
+    }
+    return array;
+  }
+  const [todos, setTodos] = useState(createBulkTodos);
   // 고유값으로 사용될 id
   // ref를 사용하여 변수에 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     text => {
